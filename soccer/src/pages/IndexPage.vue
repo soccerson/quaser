0f24b645c307f1c8b382130800aced95908a5086
Yuta-Miyakawa <af20075@shibaura-it.ac.jp>
2024-10-03 13:21:42 +0900

    create indexpage

--- soccer/src/pages/IndexPage.vue
+++ soccer/src/pages/IndexPage.vue
@@ -1,1 +1,23 @@
+<template>
+  <div>
+    <!-- ページの内容をここに記述 -->
+    <h1>Hello World!</h1>
+  </div>
+</template>
 
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      message: 'Welcome to Index Page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
