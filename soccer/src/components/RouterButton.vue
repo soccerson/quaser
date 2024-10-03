e1427f756159f92ad2067219f5666cb8802f16e6
yuuri_iwata <yuuri.iwta@gmail.com>
2024-10-03 10:55:05 +0900

    first commit

---
+++ soccer/src/components/RouterButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <router-link :to="to" class="button-wrapper">
+    <q-btn flat :label="label" :icon="icon" class="router-link-btn" />
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "RouterButton",
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      default: null, // アイコンが必要な場合に使う
+    },
+  },
+};
+</script>
+
+<style scoped>
+.router-link-btn {
+  width: auto; /* ボタンの幅を文字に自動で調整 */
+  font-weight: bold; /* フォントの強調 */
+  border-radius: 8px; /* ボタンの角を少し丸くする */
+}
+.button-wrapper {
+  text-decoration: none;
+  color: inherit;
+  height: 100%; /* 親要素の高さを全体に */
+}
+</style>
---
+++ soccer/src/pages/LoginPage.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="q-pa-md" style="max-width: 300px">
+    <form
+      @submit.prevent.stop="onSubmit"
+      @reset.prevent.stop="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        ref="nameRef"
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="nameRules"
+      />
+
+      <q-input
+        ref="ageRef"
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="ageRules"
+      />
+
+      <q-toggle v-model="accept" label="I accept the license and terms" />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </form>
+  </div>
+</template>
+
+<!-- <template>
+  <q-page class="flex flex-center">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">ログイン</div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form @submit="onSubmit">
+          <q-input v-model="email" label="Email" type="email" />
+          <q-input v-model="password" label="Password" type="password" />
+          <q-btn type="submit" label="ログイン" color="primary" />
+        </q-form>
+      </q-card-section>
+
+      <q-card-actions align="center">
+        <q-btn flat label="新規登録はこちら" @click="goToRegister" />
+      </q-card-actions>
+    </q-card>
+  </q-page>
+</template> -->
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      // ログイン処理をここに実装（例: APIへのリクエスト）
+      console.log("ログイン:", this.email, this.password);
+    },
+    goToRegister() {
+      this.$router.push("/register");
+    },
+  },
+};
+</script>
