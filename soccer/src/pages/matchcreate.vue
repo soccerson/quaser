8c07e84a5217a39651f305146476f5f44885317e
yamakazu184 <sugoroku771@gmail.com>
2024-10-03 22:00:21 +0900

    table_create

--- soccer/src/pages/matchcreate.vue
+++ soccer/src/pages/matchcreate.vue
@@ -1,11 +1,37 @@
 <template>
     <div id="app">
+    <!----- 表の表示 ----->
+        <div id="list">
+          <h5>おすすめ対戦相手</h5>
+          <table>
+            <thead>
+              <tr>
+                <th>学校名</th>
+                <th>住所</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(location, index) in locations" :key="index">
+                <td>{{location.schoolName}}</td>
+                <td>{{location.schoolAdress}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         <div id="map"></div>
     </div>
 </template>
 <script>
 export default {
   name: "SimpleMarkerMap",
+  data(){
+    return{
+      locations:[
+        {schoolName: "A学校", schoolAdress:"A市"},
+        {schoolName: "B学校", schoolAdress:"B市"}
+      ]
+    };
+  },
   mounted() {
     this.initMap();
   },
@@ -34,10 +60,27 @@ export default {
 };
 </script>
 <style>
+    table{
+      width: 80%;
+      margin: 0 auto;
+      border-collapse: collapse;
+    }
+
+    th, td{
+      border: 1px solid #ccc;
+      padding: 8px;
+      text-align: center;
+    }
+
+    #list{
+      margin-bottom: 80px;
+    }
     #map {
     height: 50vh; /* Fill the screen */
-    width: 50%;
+    width: 80%;
+    margin: 0 auto;
     }
+
     /* Always set the map height explicitly to define the size of the div
     * element that contains the map. */
     gmp-map {
